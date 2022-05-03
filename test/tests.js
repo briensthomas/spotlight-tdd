1454f57da36a2c2e817c670f05db04b6d27f2e88
Brien Thomas <thomasbrien94@gmail.com>
2022-05-03 10:28:53 -0700

    init commit

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,21 @@
 import './functions.test.js';
+test('this test should pass', (expect) => {
+    const expected = true;
+
+    const actual = myFunction();
+
+    expect.equal(actual, expected, 'true = true');
+});
+
+// This function should take a string and add return an new string that adds three exclamation points to the argument.
+/* 
+
+Input:
+
+'bunny rabbit'
+
+Output: 
+
+'bunny rabbit!!!'
+
+*/
